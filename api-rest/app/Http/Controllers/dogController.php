0d33edf1582e4a9c7ddc1e37eb30faa2486b197e
thiago-klairo <thiago.pontes20@hotmail.com>
2023-01-10 19:16:39 -0300

    Encerrando a api com as chamados do orm

--- api-rest/app/Http/Controllers/dogController.php
+++ api-rest/app/Http/Controllers/dogController.php
@@ -21,16 +21,18 @@ class dogController extends Controller
 
     public function show($id)
     {
-        //
+        return ModelsDog::find($id);
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $dog = ModelsDog::findOrFail($id);
+        $dog->update($request->all());
     }
 
     public function destroy($id)
     {
-        //
+        $dog = ModelsDog::findOrFail($id);
+        $dog->destroy($id);
     }
 }
